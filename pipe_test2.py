ff58ea197246a4abb99cad3fb59117648e13cf45
yanyonghao <yanyonghao96@gmail.com>
2024-01-31 23:05:40 +0800

    pipe测试

---
+++ pipe_test2.py
@@ -0,0 +1,18 @@
+from lmdeploy import pipeline, GenerationConfig, TurbomindEngineConfig
+
+backend_config = TurbomindEngineConfig(tp=2)
+gen_config = GenerationConfig(top_p=0.8,
+                              top_k=40,
+                              temperature=0.8,
+                              max_new_tokens=1024)
+pipe = pipeline('internlm/internlm2-chat-7b',
+                backend_config=backend_config)
+prompts = [[{
+    'role': 'user',
+    'content': 'Hi, pls intro yourself'
+}], [{
+    'role': 'user',
+    'content': 'Shanghai is'
+}]]
+for item in pipe.stream_infer(prompts, gen_config=gen_config):
+    print(item)
